d92da9941d59edf2916fa16e91d38032872887e7
gregoryverghese <greg.verghese@gmail.com>
2024-06-18 12:44:36 +0100

    added bubble overlay plot to compare bubble plots across samples

--- clonality.py
+++ clonality.py
@@ -64,3 +64,112 @@ def rare_proportion(data: Dict[str, pd.DataFrame], bound: List[int] = [1, 3, 10,
 
         result[sample] = pd.Series(rare_props)
     return result
+
+
+def bubble_overlay(data: Dict[str, pd.DataFrame], x_order: List[str], y_order: List[str], factor: str, metadata: pd.DataFrame):
+    """
+    Create a grid of bubble plots for multiple samples.
+
+    :param data: Dictionary with filenames as keys and dataframes as values.
+    :param x_order: List defining the order of x-axis categories.
+    :param y_order: List defining the order of y-axis categories.
+    :param factor: Factor determining the color of the bubbles.
+    :param metadata: DataFrame with metadata information for the samples.
+    """
+
+    for sample_name, df in data.items():
+        try:
+            sample_metadata = metadata[metadata['Sample'] == sample_name].iloc[0]
+        except:
+            continue
+        patient_protocol = f"{sample_metadata['patient_id']}"
+        
+        x_row = x_order.index(sample_metadata['pool_id'])
+        y_col = y_order.index(patient_protocol)
+
+        ax = plt.subplot2grid((len(y_order),len(x_order)), (y_col,x_row))
+        ax.set_xlim(-1, 1); ax.set_ylim(-1, 1); ax.set_aspect('equal')
+        ax.axis('off')
+
+        df = data[sample_name].head(100)
+        expansion_index = df['proportion'].max()
+        colour = [(expansion_index, 1 - expansion_index, 0)] * len(df)
+       
+        # Prepare data for circlify
+        data_prepared = [{'id': str(i), 'datum': df['count'][i]} for i in df.index]
+        circles = circ.circlify(data_prepared)
+        print('circle done')
+
+        # Add circles to the plot
+        for circle, col in zip(circles, df['col']):
+            c = Circle((circle.x, circle.y), circle.r, color=col, linewidth=0)
+            ax.add_patch(c)
+
+    ax = plt.subplot2grid((len(y_order),len(x_order)), (0,0))
+    ax.set_xlim(-1, 1); ax.set_ylim(-1, 1); ax.set_aspect('equal')
+
+    for i in range(len(y_order)):
+        plt.text(-6, -2.05*i, y_order[i], fontsize = 10)
+    for i in range(len(x_order)):
+        plt.text(2.48*i-0.5, -35, x_order[i], fontsize = 10, rotation=90)
+
+    plt.text(9, -37, 'Treatment', fontsize = 10)
+    plt.text(-7, -8, 'Patient', fontsize = 10, rotation=90)
+    ax.axis('off')
+
+
+
+def bubble_overlay(data: Dict[str, pd.DataFrame], x_order: List[str], y_order: List[str], factor: str, metadata: pd.DataFrame):
+    """
+    Create a grid of bubble plots for multiple samples.
+
+    :param data: Dictionary with filenames as keys and dataframes as values.
+    :param x_order: List defining the order of x-axis categories.
+    :param y_order: List defining the order of y-axis categories.
+    :param factor: Factor determining the color of the bubbles.
+    :param metadata: DataFrame with metadata information for the samples.
+    """
+
+    for sample_name, df in data.items():
+        print(sample_name)
+        print(metadata[metadata['Sample'] == sample_name])
+        try:
+            sample_metadata = metadata[metadata['Sample'] == sample_name].iloc[0]
+        except:
+            continue
+        patient_protocol = f"{sample_metadata['patient_id']}"
+        
+        x_row = x_order.index(sample_metadata['pool_id'])
+        y_col = y_order.index(patient_protocol)
+
+        ax = plt.subplot2grid((len(y_order),len(x_order)), (y_col,x_row))
+        ax.set_xlim(-1, 1); ax.set_ylim(-1, 1); ax.set_aspect('equal')
+        ax.axis('off')
+
+        df = data[sample_name].head(100)
+        expansion_index = df['proportion'].max()
+        colour = [(expansion_index, 1 - expansion_index, 0)] * len(df)
+       
+        # Prepare data for circlify
+        data_prepared = [{'id': str(i), 'datum': df['count'][i]} for i in df.index]
+        circles = circ.circlify(data_prepared)
+        print('circle done')
+
+        # Add circles to the plot
+        for circle, col in zip(circles, df['col']):
+            c = Circle((circle.x, circle.y), circle.r, color=col, linewidth=0)
+            ax.add_patch(c)
+
+    ax = plt.subplot2grid((len(y_order),len(x_order)), (0,0))
+    ax.set_xlim(-1, 1); ax.set_ylim(-1, 1); ax.set_aspect('equal')
+
+    for i in range(len(y_order)):
+        plt.text(-6, -2.05*i, y_order[i], fontsize = 10)
+    for i in range(len(x_order)):
+        plt.text(2.48*i-0.5, -35, x_order[i], fontsize = 10, rotation=90)
+
+    plt.text(9, -37, 'Treatment', fontsize = 10)
+    plt.text(-7, -8, 'Patient', fontsize = 10, rotation=90)
+    ax.axis('off')
+
+
